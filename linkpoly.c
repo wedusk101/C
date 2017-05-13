f5bd881538939299b54384982add6e3914ce9da6
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2017-05-14 05:11:36 +0530

    Polynomial using linked list

---
+++ linkpoly.c
@@ -0,0 +1,99 @@
+/*The following program implements the representation of a polynomial using linked lists.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int coeff;
+	int exp;
+	struct node *next;
+};
+
+typedef struct node* NODEPTR;
+
+void initList(NODEPTR*);
+void insertEndList(NODEPTR*, NODEPTR*); 
+void displayPolynomial(NODEPTR);
+
+int main()
+{
+	NODEPTR list, secondlist, lastNode;
+	initList(&list);
+	printf("\nPlease enter the terms of the polynomial in the following format:\n");
+	printf("(a_n * x ^ n) + (a_n-1 * x ^ n-1) + ... + (a_1 * x) + a_0\n");
+	printf("\nWhere a_n to a_0 represent the coefficients in descending order \nand ^ n, ^ n-1 and so on represent the coefficients in descending order.\n\n");
+	insertEndList(&list, &lastNode);
+	printf("The polynomial is ");
+	displayPolynomial(list);
+	printf(" .\n");
+}
+
+void initList(NODEPTR *plist)
+{
+	*plist = NULL;
+}
+
+void insertEndList(NODEPTR *plist, NODEPTR *lastNode)
+{
+	NODEPTR newNode, tmp;
+    int cf = 0, ex = 0;
+    char ch;
+	tmp = *plist;
+    do
+	{
+		printf("Please enter the coefficient.\n");
+        scanf("%d", &cf);
+		printf("Please enter the exponent.\n");
+        scanf("%d", &ex);
+        newNode = malloc(sizeof(struct node));
+        newNode->coeff = cf;
+		newNode->exp = ex;
+        newNode->next = NULL;
+		if(*plist == NULL)
+		{
+			*plist = newNode;
+			*lastNode = newNode;
+		}
+        else
+			(*lastNode)->next = newNode;
+		printf("Value inserted successfully.\n");
+        printf("Do you wish to continue? (Y/N)\n");
+        scanf(" %c", &ch);
+        *lastNode = newNode;
+    }while(ch == 'y' || ch == 'Y');
+}
+
+void displayPolynomial(NODEPTR plist)
+{
+	if(plist == NULL)
+	{
+		printf("First create a polynomial! Operation aborted.\n");
+		return;
+	}
+	while(plist != NULL)
+	{
+		if(plist->exp == 0)
+		{
+			printf("%d", plist->coeff);
+			return;
+		}
+		if(plist -> next == NULL)
+		{
+			printf("%dx^%d", plist->coeff, plist->exp);
+			return;
+		}
+		if(plist -> coeff != 0 && (plist->exp != 0 && plist->exp != 1))
+		{
+			printf("%dx^%d + ", plist->coeff, plist->exp);
+		}
+		if(plist->coeff == 1)
+		{
+			printf("x^%d + ", plist->exp);
+		}
+		if(plist->coeff != 0 && plist->exp == 1)
+		{
+			printf("%d^x + ", plist->coeff);
+		}
+		plist = plist->next;
+	}
+}
