fd56385960da545c238067ce5ff4f420534e29f4
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2017-07-08 18:16:06 +0530

    Point Quad Tree

---
+++ ptquadtree.c
@@ -0,0 +1,364 @@
+/*The following code is work in progess and implements a point quad tree with a few of its basic operations.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+struct point
+{
+	float x;
+	float y;
+	char name[10];
+};
+
+struct treeNode
+{
+	struct point coo;
+	struct treeNode *nw;
+	struct treeNode *ne;
+	struct treeNode *se;
+	struct treeNode *sw;
+};
+
+typedef struct treeNode *NODEPTR;
+
+void initTree(NODEPTR*); 
+int searchPQT(NODEPTR, NODEPTR*, float, float);
+//nearest neighbor search
+//distance radius search
+void relDirection(NODEPTR, NODEPTR*, float, float, float, float);
+void insertNodePQT(NODEPTR*, char*, float, float);
+void displayTree(NODEPTR);
+float calcDistance(NODEPTR, NODEPTR*, float, float, float, float);
+int countNodePQT(NODEPTR);
+int countLeafPQT(NODEPTR);
+
+int main()
+{
+	NODEPTR center, index;
+	center = NULL;
+	initTree(&center);
+	index = NULL;
+	int choice = 0;
+	float key_x = 0, key_y = 0, key_x2, key_y2;
+	char inputName[10];
+	printf("This program implements a point quad tree with the following basic operations.\n");
+	do
+	{
+		printf("\n----------- MENU -------------\n");
+        printf("\n 1.	Insert a point into the point quad tree.\n");
+		printf("\n 2.	Search for an element in the binary search tree.\n");
+		printf("\n 3.	Display the contents of the point quad tree.\n");
+		printf("\n 4.	Calculate the Euclidean distance between two points in the tree.\n");
+		printf("\n 5.	Find out the relative direction between two points in the tree.\n");
+		printf("\n 6.	Count the number of nodes in the tree.\n");
+		printf("\n 7.	Count the number of leaf nodes in the tree.\n");
+		printf("\n 0.	EXIT \n");
+        printf("\n------------------------------\n");
+        printf("\nPlease enter your choice : ");
+        scanf("%d",&choice);
+		switch(choice)
+		{	case 1: 	printf("\nPlease enter the point as x-y coordinate tuple.\n");
+						scanf("%f",&key_x);
+						scanf("%f",&key_y);
+						printf("Please enter a name for the point. No two points should have the same name.\n");
+						setbuf(stdin, NULL);	// removes pending newline from the buffer from the last input
+						fgets(inputName, 10, stdin);
+						strtok(inputName, "\n");	// removes pending newline from the buffer after the string input
+						insertNodePQT(&center, inputName, key_x, key_y);
+						break;
+			
+			case 2:		printf("Please enter the point to search for.\n");
+						scanf("%f",&key_x);
+						scanf("%f",&key_y);
+						if(searchPQT(center, &index, key_x, key_y) == TRUE)
+							printf("Element found!\n");
+						else
+							printf("Element not found.\n");
+						break;
+					
+			case 3:		if(center == NULL)
+						{
+							printf("Tree is empty!");
+							break;
+						}
+						else
+						{	printf("The points in the plane are:\n");
+							displayTree(center);
+						}
+						break;
+						
+			case 4:		printf("Please enter the first point.\n");
+						scanf("%f",&key_x);
+						scanf("%f",&key_y);
+						printf("Please enter the second point.\n");
+						scanf("%f",&key_x2);
+						scanf("%f",&key_y2);
+						if(searchPQT(center, &index, key_x, key_y) == TRUE	&& searchPQT(center, &index, key_x2, key_y2) == TRUE)
+							printf("The distance between the two points is %4.2f units.", calcDistance(center, &index, key_x, key_y, key_x2, key_y2));
+						else
+							printf("One or both points is/are not in the tree. Operation aborted.\n");							
+						break;
+						
+			case 5:		printf("Please enter the coordinates of the source point\n");
+						scanf("%f",&key_x);
+						scanf("%f",&key_y);
+						printf("Please enter the coordinates of the destination point\n");
+						scanf("%f",&key_x2);
+						scanf("%f",&key_y2);
+						relDirection(center, &index, key_x, key_y, key_x2, key_y2);
+						break;
+						
+			case 6:		printf("The number of nodes in the tree is %d.\n", countNodePQT(center));
+						break;
+						
+			case 7:		printf("The number of leaf nodes in the tree is %d.\n", countLeafPQT(center));
+						break;
+						
+			case 0:		printf("Thank you.\n");
+						break;
+					
+			default:	printf("\nInvalid choice. Try again.\n");
+		}
+	}while(choice != 0);
+} // end
+
+void insertNodePQT(NODEPTR *proot, char* pname, float valx, float valy) // recursively inserts a point into the tree
+{
+	NODEPTR tmp;
+	tmp = malloc(sizeof(struct treeNode));
+	tmp->coo.x = valx;
+	tmp->coo.y = valy;
+	strncpy(tmp->coo.name, pname, 10);
+	tmp->nw = tmp->ne = tmp->se = tmp->sw = NULL;
+	if(*proot == NULL)
+	{
+		*proot = tmp;
+		printf("Point inserted succesfully.\n");
+		return;
+	}
+	if(valx > (*proot)->coo.x)
+	{
+		if(valy > (*proot)->coo.y)
+			insertNodePQT(&(*proot)->ne, &(*pname), valx, valy);
+		else if(valy < (*proot)->coo.y)
+			insertNodePQT(&(*proot)->se, &(*pname), valx, valy);			
+	}
+	else if(valx < (*proot)->coo.x)
+	{
+		if(valy > (*proot)->coo.y)
+			insertNodePQT(&(*proot)->nw, &(*pname), valx, valy);
+		else if(valy < (*proot)->coo.y)
+			insertNodePQT(&(*proot)->sw, &(*pname), valx, valy);
+	}
+	else if(valx == (*proot)->coo.x) // handles multiple points on an axis with the same x coordinate
+	{
+		if(valy > (*proot)->coo.y)
+		{
+			if((*proot)->nw == NULL)
+			{
+				insertNodePQT(&(*proot)->nw, &(*pname), valx, valy);
+				return;
+			}
+			if((*proot)->ne == NULL)
+			{
+				insertNodePQT(&(*proot)->ne, &(*pname), valx, valy);
+				return;
+			}
+			printf("Error occured! Point could not be inserted."); // this condition needs to be handled somehow
+			return;			
+		}
+		else if(valy < (*proot)->coo.y)
+		{
+			if((*proot)->sw != NULL)
+			{
+				insertNodePQT(&(*proot)->sw, &(*pname), valx, valy);
+				return;
+			}
+			if((*proot)->se != NULL)
+			{
+				insertNodePQT(&(*proot)->se, &(*pname), valx, valy);
+				return;
+			}
+			printf("Error occured! Point could not be inserted."); // this condition needs to be handled somehow
+			return;
+		}
+	}
+	else if(valy == (*proot)->coo.y)
+	{
+		if(valx > (*proot)->coo.x)
+		{
+			if((*proot)->ne != NULL)
+			{
+				insertNodePQT(&(*proot)->ne, &(*pname), valx, valy);
+				return;
+			}
+			if((*proot)->se != NULL)
+			{
+				insertNodePQT(&(*proot)->ne, &(*pname), valx, valy);
+				return;
+			}
+			printf("Error occured! Point could not be inserted."); 				
+		}
+		else if(valx > (*proot)->coo.x)
+		{
+			if((*proot)->nw != NULL)
+			{
+				insertNodePQT(&(*proot)->nw, &(*pname), valx, valy);
+				return;
+			}
+			if((*proot)->sw != NULL)
+			{
+				insertNodePQT(&(*proot)->sw, &(*pname), valx, valy);
+				return;
+			}
+			printf("Error occured! Point could not be inserted.");
+		}
+	}
+	else if(valx == (*proot)->coo.x	&& valy == (*proot)->coo.y)
+	{
+		printf("Operation failed! Point alreasy exists.\n");
+		return;
+	}
+	 
+}
+
+void initTree(NODEPTR* proot) // initiliazes the tree with (0,0) as the origin
+{
+	*proot = malloc(sizeof(struct treeNode));
+	(*proot)->coo.x = 0;
+	(*proot)->coo.y = 0;
+	strncpy((*proot)->coo.name, "O", 10);
+	(*proot)->nw = NULL;
+	(*proot)->ne = NULL;
+	(*proot)->se = NULL;
+	(*proot)->sw = NULL;	
+}
+
+void displayTree(NODEPTR root) // recursively displays the contents of the tree
+{
+	if(root != NULL)
+	{
+		printf("%s(%4.2f,%4.2f) ", root->coo.name, root->coo.x, root->coo.y);
+		displayTree(root->nw);
+		displayTree(root->ne);
+		displayTree(root->se);
+		displayTree(root->sw);
+	}
+}
+
+int searchPQT(NODEPTR root, NODEPTR* index, float valx, float valy) // recursively searches the tree for a point
+{
+	if(root == NULL)
+		return FALSE;
+	if((root->coo.x) == valx && (root->coo.y) == valy)
+	{
+		*index = root;	// returns a pointer to the found element
+		return TRUE;
+	}
+	if(valx > (root->coo.x))
+	{
+		if(valy > (root->coo.y))
+			searchPQT(root->ne, index, valx, valy);
+		else if(valy < (root->coo.y))
+			searchPQT(root->se, index, valx, valy);
+	}
+	else if(valx < (root->coo.x))
+	{
+		if(valy > (root->coo.y))
+			searchPQT(root->nw, index, valx, valy);
+		else if(valy < (root->coo.y))
+			searchPQT(root->sw, index, valx, valy);
+	}
+}
+
+float calcDistance(NODEPTR root, NODEPTR* index, float valx1, float valy1, float valx2, float valy2)
+{
+	return sqrt(pow(valx1 - valx2, 2) + pow(valy1 - valy2, 2));
+}
+
+void relDirection(NODEPTR root, NODEPTR* index, float valx1, float valy1, float valx2, float valy2)
+{
+	if(searchPQT(root, index, valx1, valy1) == TRUE	&& searchPQT(root, index, valx2, valy2) == TRUE)
+	{
+		if(valx2 > valx1)
+		{
+			if(valy2 > valy1)
+			{
+				printf("The destination lies to the north-east of the source.\n");
+				return;
+			}
+			else if(valy2 < valy1)
+			{
+				printf("The destination lies to the south-east of the source.\n");
+				return;
+			}
+		}
+		if(valx2 < valx1)
+		{
+			if(valy2 > valy1)
+			{
+				printf("The destination lies to the north-west of the source.\n");
+				return;
+			}
+			else if(valy2 < valy1)
+			{
+				printf("The destination lies to the south-west of the source.\n");
+				return;
+			}
+		}
+		if(valx1 == valx2)
+		{
+			if(valy2 > valy1)
+			{
+				printf("The destination lies to the north of the source.\n");
+				return;
+			}
+			else if(valy2 < valy1)
+			{
+				printf("The destination lies to the south of the source.\n");
+				return;
+			}			
+		}
+		if(valy1 == valy2)
+		{
+			if(valx2 > valx1)
+			{
+				printf("The destination lies to the east of the source.\n");
+				return;
+			}
+			else if(valy2 < valy1)
+			{
+				printf("The destination lies to the west of the source.\n");
+				return;
+			}
+		}
+		
+	}
+	else
+		printf("One or both points is/are not in the tree.Operayion aborted.\n");
+}
+
+int countNodePQT(NODEPTR root) // recursively counts the number of nodes in the tree
+{
+	if(root	!=	NULL)
+		return 1 + countNodePQT(root->nw) + countNodePQT(root->ne) + countNodePQT(root->se) + countNodePQT(root->sw);
+	else
+		return 0;
+}
+
+int countLeafBST(NODEPTR root) // recursively counts the leaf nodes in the tree
+{
+	if(root == NULL)
+		return 0;
+	if(root != NULL)
+	{
+		if(root->nw == NULL && root->ne == NULL && root->se == NULL && root->sw == NULL)
+			return 1;
+		else
+			return countLeafPQT(root->nw) + countLeafPQT(root->ne) + countLeafPQT(root->se)	+ countLeafPQT(root->sw);	
+	}
+}
