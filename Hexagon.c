013f700118cb48d1de9f051df7f9ebe7ed0ad5dd
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-01-25 04:00:33 +0530

    Update

--- Hexagon.c
+++ Hexagon.c
@@ -3,9 +3,9 @@
 int main() // prints a crude hexagon
 {
 	int i = 0, j = 0;
-	for(i = 0; i < 100; i++) // y - axis
+	for(i = 0; i < 20; i++) // y - axis
 	{
-		for(j = 0; j < 100; j++) // x - axis
+		for(j = 0; j < 20; j++) // x - axis
 		{
 			if(i <= 9 && i >= 1 && (2*j) + i <= 25 && (2*j) + i >= 9 && (2*j) - i + 1 >= 0 && (2*j) - i <= 15)
 				printf("*");
