6b7dc621202a2b0effa1c524aa166c0a351ee5b7
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-02-26 19:33:59 +0530

    Update

--- recNumRvrs.c
+++ recNumRvrs.c
@@ -2,24 +2,22 @@
 #include <math.h>
 
 int recNumRvrs(int, int);
-int countDigits(int);
+void countDigits(int, int*);
 
 int main()
 {
 	int input = 0, digits = 0;
 	printf("Please enter a positive integer.\n");
 	scanf("%d", &input);
-	digits = countDigits(input);
+	countDigits(input, &digits);
 	printf("The reverse of %d is %d.\n", input, recNumRvrs(input, digits));
 	return 0;
 }
 
-int countDigits(int num)
+void countDigits(int num, int *count)
 {
-	int count = 0;
 	for(;num != 0; num /= 10)
-		count++;
-	return count;
+		(*count)++;
 }
 
 int recNumRvrs(int num, int digits)
