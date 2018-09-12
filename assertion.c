fe7f0fce172d5ff8518f54a0af3d9b2d0c4250ac
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-09-13 03:40:59 +0530

    Assertion macro update

--- assertion.c
+++ assertion.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+// #define NDEBUG  // used for disabling assertions for release code
 #include <assert.h>
 
 int main()
@@ -6,7 +7,7 @@ int main()
 	int x = 0;
 	printf("Please enter an even number.\n");
 	scanf("%d", &x);
-	assert(x % 2 == 0 && "ERROR: Input number is NOT prime. Program terminated."); // assert(condition && message) in case the assertion fails
+	assert(x % 2 == 0 && "ERROR: Input number is NOT even. Program terminated."); // assert(condition && message) in case the assertion fails
 	printf("Okay\n"); // assertion passes
 	return 0;
 }
