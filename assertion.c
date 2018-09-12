066b918606f28bd27edf912542c619fdb6ce73a0
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-09-12 13:59:29 +0530

    Assertion update

--- assertion.c
+++ assertion.c
@@ -3,8 +3,10 @@
 
 int main()
 {
-	int x = 4;
-	assert(x > 2);
-	printf("\n%d\n", x);
+	int x = 0;
+	printf("Please enter an even number.\n");
+	scanf("%d", &x);
+	assert(x % 2 == 0 && "ERROR: Input number is NOT prime. Program terminated."); // assert(condition && message) in case the assertion fails
+	printf("Okay\n"); // assertion passes
 	return 0;
 }
